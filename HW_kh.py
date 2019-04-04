24ab90fcd9a535bcb7759847cb50c1968629fcfd
kyunghee28 <49008604+kyunghee28@users.noreply.github.com>
2019-04-04 09:30:40 +0900

    HW

---
+++ HW_kh.py
@@ -0,0 +1,38 @@
+import matplotlib.pyplot as plt
+import numpy as np
+#숙제)2016_GDP.txt 파일을 읽어 들여 상위 10개의 나라를 막대그래프로
+
+info = []
+with open('./Data/2016_GDP.txt','r',encoding='utf-8') as fp:
+    for line in fp:
+        line = line.strip().replace(",","")
+        line = line.split(':')
+        info.append(line)
+        # print(line)
+
+del info[0]
+# print(info)
+
+c_name = []
+c_gdp = []
+for g in info:
+    c_name.append(g[1])
+    c_gdp.append(g[2])
+# print(c_gdp)
+
+c_name2 = np.array(c_name)
+c_gdp2 = np.array(c_gdp, dtype=np.int)
+
+gdp2 =np.argsort(c_gdp2)[::-1] # 정렬
+n = c_name2[:10]
+# print(gdp2,c_gdp2[:10])
+# print(n)
+
+from matplotlib import font_manager, rc
+
+rc('font', family=font_manager.FontProperties(fname="C:/Windows/Fonts/malgun.ttf").get_name())
+
+plt.bar(n,c_gdp2[:10], color = "pink")
+
+plt.title("2016년 GDP_TOP10")
+plt.show()
